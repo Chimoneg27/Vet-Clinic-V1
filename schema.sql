dd3f41e912026361a3279893acd7a59b981d9c43
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-24 17:39:30 +0200

    Add the data, schema and queries sql files

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255),
+	date_of_birth DATE,
+    escape_attempts INT,
+	neutered BOOLEAN,
+	weight_kg DECIMAL(6, 2)
+);
