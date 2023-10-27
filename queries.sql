a45b5d7cf8803ea6ff9d86aeab1c28ba980d1190
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-27 12:00:39 +0200

    Add queries for visits, vets and specializations

--- queries.sql
+++ queries.sql
@@ -124,3 +124,43 @@ FROM owners o
 JOIN animals a ON o.id = a.owners
 GROUP BY o.full_name 
 ORDER BY count(a.id) DESC;
+
+--queries for Visits
+
+SELECT a.name 
+FROM animals a JOIN visits v ON v.animal_id = a.id 
+JOIN vets ve ON ve.id = v.vet_id 
+WHERE ve.name = 'William Tatcher' 
+ORDER BY v.date_of_visit DESC
+LIMIT 1;
+
+SELECT COUNT(DISTINCT a.id) AS num_animals_seen
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets ve ON v.vet_id = ve.id
+WHERE ve.name = 'Stephanie Mendez';
+
+SELECT ve.name, s.name 
+from vets ve LEFT JOIN specializations spe ON spe.vet_id = ve.id 
+LEFT JOIN species s ON s.id = spe.species_id;
+
+SELECT a.name, COUNT(*) AS num_visits
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+GROUP BY a.id
+ORDER BY num_visits DESC
+LIMIT 1;
+
+SELECT count(v.id) from visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets ve ON v.vet_id = ve.id
+LEFT JOIN specializations spe ON spe.vet_id = ve.id AND spe.species_id = a.species_id
+WHERE spe.id IS NULL;
+
+SELECT s.name FROM animals a
+JOIN species s ON a.species_id = s.id
+JOIN visits v ON v.animal_id = a.id
+JOIN vets ve ON ve.id = v.vet_id
+where ve.name = 'Maisy Smith'
+GROUP BY s.name
+ORDER BY count(a.id) DESC;
