0c38a3168d950d429f2d80caa3ceb80aaa0698c9
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-10-27 11:59:59 +0200

    Add table for vets, specializations and visits

--- schema.sql
+++ schema.sql
@@ -26,3 +26,23 @@ ADD COLUMN owners integer REFERENCES owners(id);
 
 ALTER TABLE animals
 ADD COLUMN species integer REFERENCES species(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    id serial PRIMARY KEY,
+    vet_id integer REFERENCES vets(id),
+    species_id integer REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    animal_id integer REFERENCES animals(id),
+    vet_id integer REFERENCES vets(id),
+    date_of_visit DATE
+);
